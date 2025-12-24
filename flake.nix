c22ce671c2814f8fc3ea21d8e3fb4217343f7f4c
NacreousDawn596 <kamal@aferiad.xyz>
2025-12-24 03:58:33 +0100

    fixed version mismatch

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "nixboost";
-          version = "1.0.4";
+          version = "1.0.5";
 
           src = ./.;
 
